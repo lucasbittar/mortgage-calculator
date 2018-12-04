ec386bbb4be34db5b5790f3a68285d0b8d2a7347
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2018-12-04 00:04:32 -0200

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+// ConsumerAffairs - Main Javascript file
+
+console.log('Working!')
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+  },
+};
